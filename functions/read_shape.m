3cc61308ad2ee6a063417c3f00701d61bea898fb
LiyouWang <1085066875@qq.com>
2019-09-26 11:56:47 +0800

    demo

---
+++ functions/read_shape.m
@@ -0,0 +1,18 @@
+function [shape] = read_shape(shape_path, n_vertices)
+
+% read_shape pts
+disp(shape_path)
+fid = fopen(shape_path);
+tline = fgetl(fid);
+start = 1;
+while ~strcmp(tline, '{')
+%     disp(tline);
+    start = start + 1;
+    tline = fgetl(fid);
+end
+fclose(fid);
+
+% read shape
+shape =  dlmread(shape_path, ' ', [start 0 start+n_vertices-1 1]);
+end
+
